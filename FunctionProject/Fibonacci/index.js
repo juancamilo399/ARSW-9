23be867ae7a7e3c25281cf737f4c065e2fd4cac5
juancamilo399 <juan.angel-h@mail.escuelaing.edu.co>
2020-11-12 14:45:16 -0500

    img

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,25 +1,26 @@
-var bigInt = require("big-integer");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
-    let answer = bigInt.zero;
+    
+    var memo = {};
 
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+    memo[0] = 0;
+    memo[1] = 1;
+
+    function fib(n) {
+        var sign = n >= 0 ? 1 : -1;
+        n = Math.abs(n);
+
+        // If we already calculated the value, just use the same
+        if(memo[n] !== undefined)
+            return sign*memo[n];
+
+        // Else we will calculate it and store it and also return it
+        return sign*(memo[n] = fib(n-1)+fib(n-2));
     }
+    
+    let answer = fib(nth)
 
     context.res = {
         body: answer.toString()
